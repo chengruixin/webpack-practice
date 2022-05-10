509c850a2f07dbb2910cef0040bc5b03af05b68d
chengruixin <chengruixin_2010@163.com>
2022-05-10 16:33:23 +0800

    public path & static configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,28 @@
 const path = require("path");
 const HTMLWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
   mode: "development",
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "main.js",
+    publicPath: '/', // is only webpacked js file
   },
-
+  devServer: {
+    static: [
+      {
+        directory: path.resolve(__dirname, 'static'),
+        publicPath: '/assets', // this is for all static resources prefix
+      }
+    ], // 静态资源文件根目录. e.g dist/show.txt will be accessed by /show.txt
+    compress: true, // 是否启动压缩
+    port: 3691,
+    open: true, // 自动打开浏览器
+    devMiddleware: {
+      writeToDisk: false,
+    }
+  },
   module: {
     rules: [
       {
